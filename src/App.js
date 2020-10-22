163af04e5f898a55e1d719323b65dc58f7470468
ufuk <ufuk.uysl5@gmail.com>
2020-10-22 14:06:30 +0200

    create Homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Homepage from "./components/Homepage"
 
 import './App.css';
 
 function App() {
   return (
     <div >
-
+      <Homepage />
     </div >
   );
 }
