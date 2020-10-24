f8fd84529b6ba712e30ba9876b93972830e49b41
ufuk <ufuk.uysl5@gmail.com>
2020-10-24 12:30:53 +0200

    created new page and signIn-SignUp components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import './App.css';
 
 import HomePage from './pages/Homepage';
 import ShopPage from './pages/ShopPage/Shop';
+import SignInAndSignUpPage from "./pages/SignIn-SignUp/SignInAndSignUpPage"
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
